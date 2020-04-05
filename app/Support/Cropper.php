5441337930142fad1616ed4c1fb69b593b1d5c9e
raphaelmoraes89 <raphael.moraes@mls.com.br>
2020-04-04 21:09:27 -0300

    Nada demail

--- app/Support/Cropper.php
+++ app/Support/Cropper.php
@@ -13,7 +13,7 @@ class Cropper
 		$cropper = new \CoffeeCode\Cropper\Cropper("../public/storage/cache");
 		$pathThumb = $cropper->make(config('filesystems.disks.public.root') . "/" . $imagePathUri, $width, $height);
 		// se for php puro é só utilizar o array pop para pegar a ultima posicao, porém 
-		// aqui utilizarei um recurso do laravel que é o collect
+		// aqui utilizarei um recurso do laravel que é o collect !!!
 		return $file = 'cache/' . collect(explode('/', $pathThumb))->last();
 
 	}
